6370f4ddd816402a760f87151606ad54b567f17b
WJ1241 <83521037+WJ1241@users.noreply.github.com>
2022-01-20 17:19:40 +0000

    (Will) 17:15 (Finished CommandScheduler) - Added ICommandScheduler - Added IInitialiseICommandScheduler - Added IInitialiseIFactory - Edited EntityManager - Edited SceneManager - Edited CommandScheduler - Edited CommandOneParam - Edited ICommandOneParam

---
+++ COMP3451Project/EnginePackage/Services/Factories/IInitialiseIFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP3451Project.EnginePackage.Services.Factories
+{
+    /// <summary>
+    /// Interface which initialises an object with an IFactory<T> object
+    /// Author: William Smith & Declan Kerby-Collins
+    /// Date: 20/01/22
+    /// </summary>
+    /// <typeparam name="A"> Any Class to make a factory for </typeparam>
+    public interface IInitialiseIFactory<A>
+    {
+        #region METHODS
+
+        /// <summary>
+        /// Initialises an object with an IFactory<A> object
+        /// </summary>
+        /// <param name="pFactory"> IFactory<A> object </param>
+        void Initialise(IFactory<A> pFactory);
+
+        #endregion
+    }
+}
