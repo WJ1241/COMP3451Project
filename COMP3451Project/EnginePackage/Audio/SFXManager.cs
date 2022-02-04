bf237f61ff0733e24f44e4d05aea28118538d2f5
WJ1241 <83521037+WJ1241@users.noreply.github.com>
2022-02-04 20:39:18 +0000

    (Will) 20:38 - Added IPlayAudio - Added SFXManager - Added SongManager

---
+++ COMP3451Project/EnginePackage/Audio/SFXManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework.Audio;
+using COMP3451Project.EnginePackage.CoreInterfaces;
+
+namespace COMP3451Project.EnginePackage.Audio
+{
+    /// <summary>
+    /// Class which stores sound effects and plays a requested sound effect when needed
+    /// Authors: William Smith & Declan Kerby-Collins
+    /// Date: 04/02/22
+    /// </summary>
+    public class SFXManager : IInitialiseParam<string, SoundEffect>, IPlayAudio
+    {
+        #region FIELD VARIABLES
+
+        // DECLARE an IDictionary<string, SoundEffect>, name it '_sfxDict':
+        private IDictionary<string, SoundEffect> _sfxDict;
+
+        #endregion
+
+
+        #region CONSTRUCTOR
+
+        /// <summary>
+        /// Constructor for objects of SFXManager
+        /// </summary>
+        public SFXManager()
+        {
+            // INSTANTIATE _sfxDict as a new Dictionary<string, SoundEffect>():
+            _sfxDict = new Dictionary<string, SoundEffect>();
+        }
+
+        #endregion
+
+
+        #region IMPLEMENTATION OF IINITIALISEPARAM<STRING, SOUNDEFFECT>
+
+        /// <summary>
+        /// Initialises an object with a string and a SoundEffect
+        /// </summary>
+        /// <param name="pSFXName"> Name of Sound Effect </param>
+        /// <param name="pSFXFile"> Sound Effect File </param>
+        public void Initialise(string pSFXName, SoundEffect pSFXFile)
+        {
+            // ADD pSFXName as a key, and pSFXFile as a value to _sfxDict:
+            _sfxDict.Add(pSFXName, pSFXFile);
+        }
+
+        #endregion
+
+
+        #region IMPLEMENTATION OF IPLAYAUDIO
+
+        /// <summary>
+        /// Plays an audio clip related to the string parameter, either an effect or a song 
+        /// </summary>
+        /// <param name="pSoundFile"> Name of the Audio File to play </param>
+        public void PlayAudio(string pSoundFile)
+        {
+            // PLAY sound effect addressed as value of pSoundFile in _sfxDict:
+            _sfxDict[pSoundFile].Play();
+        }
+
+        #endregion
+    }
+}
---
+++ COMP3451Project/EnginePackage/Audio/SongManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework.Media;
+using COMP3451Project.EnginePackage.CoreInterfaces;
+
+namespace COMP3451Project.EnginePackage.Audio
+{
+    /// <summary>
+    /// Class which stores songs and plays a requested song when needed
+    /// Authors: William Smith & Declan Kerby-Collins
+    /// Date: 04/02/22
+    /// </summary>
+    public class SongManager : IInitialiseParam<string, Song>, IPlayAudio
+    {
+        #region FIELD VARIABLES
+
+        // DECLARE an IDictionary<string, Song>, name it '_songDict':
+        private IDictionary<string, Song> _songDict;
+
+        #endregion
+
+
+        #region CONSTRUCTOR
+
+        /// <summary>
+        /// Constructor for objects of SongManager
+        /// </summary>
+        public SongManager()
+        {
+            // INSTANTIATE _songDict as a new Dictionary<string, Song>():
+            _songDict = new Dictionary<string, Song>();
+        }
+
+        #endregion
+
+
+        #region IMPLEMENTATION OF IINITIALISEPARAM<STRING, SONG>
+
+        /// <summary>
+        /// Initialises an object with a string and a Song
+        /// </summary>
+        /// <param name="pSongName"> Name of Song </param>
+        /// <param name="pSongFile"> Song File </param>
+        public void Initialise(string pSongName, Song pSongFile)
+        {
+            // ADD pSongName as a key, and pSongFile as a value to _sfxDict:
+            _songDict.Add(pSongName, pSongFile);
+        }
+
+        #endregion
+
+
+        #region IMPLEMENTATION OF IPLAYAUDIO
+
+        /// <summary>
+        /// Plays an audio clip related to the string parameter, either an effect or a song 
+        /// </summary>
+        /// <param name="pSoundFile"> Name of the Audio File to play </param>
+        public void PlayAudio(string pSoundFile)
+        {
+            // PLAY song addressed as pSoundFile in _songDict using MediaPlayer:
+            MediaPlayer.Play(_songDict[pSoundFile]);
+        }
+
+        #endregion
+    }
+}
