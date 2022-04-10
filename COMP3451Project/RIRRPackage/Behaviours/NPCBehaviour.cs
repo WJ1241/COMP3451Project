fed14d18598f26f26770f3c6b8d54750c42b3c79
Declan <declankerbycollins@yahoo.co.uk>
2022-04-10 20:27:22 +0100

    dec 8:27 commented NPC behvaior

--- COMP3451Project/RIRRPackage/Behaviours/NPCBehaviour.cs
+++ COMP3451Project/RIRRPackage/Behaviours/NPCBehaviour.cs
@@ -1,6 +1,8 @@
-﻿using OrbitalEngine.Behaviours.Interfaces;
+﻿using Microsoft.Xna.Framework;
+using OrbitalEngine.Behaviours.Interfaces;
 using OrbitalEngine.CoreInterfaces;
 using OrbitalEngine.CustomEventArgs;
+using OrbitalEngine.EntityManagement.Interfaces;
 using OrbitalEngine.Services.Commands.Interfaces;
 
 namespace COMP3451Project.RIRRPackage.Behaviours
@@ -16,7 +18,8 @@ namespace COMP3451Project.RIRRPackage.Behaviours
 
         // DECLARE an ICommand, name it '_sfxCommand':
         private ICommand _sfxCommand;
-
+        // DECLARE an int, name it '_tileSize':
+        int _tileSize = 16;
         #endregion
 
 
@@ -127,6 +130,13 @@ namespace COMP3451Project.RIRRPackage.Behaviours
         /// <param name="pArgs"> CollisionEventArgs object </param>
         public void OnEvent(object pSource, CollisionEventArgs pArgs)
         {
+
+
+            // ASIGNMENT _tileSize has its value multiplied by 3, this way we have the value of 3 tiles
+            // and can be used for the tile height or width:
+            _tileSize = _tileSize * 3;
+
+            //------------------------------------ Entity Collision, for colliding with objects -------------------
             // IF moving left:
             if ((_entity as IVelocity).Velocity.X < 0)
             {
@@ -138,34 +148,65 @@ namespace COMP3451Project.RIRRPackage.Behaviours
                 //    _velocity.X = _velocity.X - 0.2f * (pArgs.RequiredArg as IVelocity).Velocity.Length();
                 //}
 
+                // IF _entity(NPC) velocity.X is greater than or equal to pArgs(Player) textureSize.X minus 1:
                 if ((_entity as IVelocity).Velocity.X >= (pArgs.RequiredArg as ITexture).TextureSize.X - 1)
                 {
+                    // ASSIGNMENT _velocitcy.X is set to 0:
                     _velocity.X -= _velocity.X;
                 }
+
+                // MULTIPLY _currentVel.X by '-1':
+                _velocity.X *= -1;
+
+                // APPLY new Velocity to _entity.Velocity:
+                (_entity as IVelocity).Velocity = _velocity;
             }
             // IF moving right:
             else if ((_entity as IVelocity).Velocity.X > 0)
             {
+                // IF _entity(NPC) velocity.X is less than or equal to pArgs(Player) textureSize.X pluss 1:
                 if ((_entity as IVelocity).Velocity.X <= (pArgs.RequiredArg as ITexture).TextureSize.X + 1)
                 {
+                    // ASSIGNMENT _velocitcy.X is set to 0:
                     _velocity.X -= _velocity.X;
                 }
+
+                // MULTIPLY _currentVel.X by '-1':
+                _velocity.X *= -1;
+
+                // APPLY new Velocity to _entity.Velocity:
+                (_entity as IVelocity).Velocity = _velocity;
             }
             // IF moving top:
             else if ((_entity as IVelocity).Velocity.Y < 0)
             {
+                // IF _entity(NPC) velocity.Y is greater than or equal to pArgs(Player) textureSize.X  minus 1:
                 if ((_entity as IVelocity).Velocity.Y >= (pArgs.RequiredArg as ITexture).TextureSize.Y - 1)
                 {
+                    // ASSIGNMENT _velocity.Y is set to 0:
                     _velocity.Y -= _velocity.Y;
                 }
+
+                // MULTIPLY _currentVel.Y by '-1':
+                _velocity.Y *= -1;
+
+                // APPLY new Velocity to _entity.Velocity:
+                (_entity as IVelocity).Velocity = _velocity;
             }
             // IF moving bottom:
             else if ((_entity as IVelocity).Velocity.Y > 0)
             {
                 if ((_entity as IVelocity).Velocity.Y <= (pArgs.RequiredArg as ITexture).TextureSize.Y + 1)
                 {
+                    // ASSIGNMENT _velocity.Y is set to 0:
                     _velocity.Y -= _velocity.Y;
                 }
+
+                // MULTIPLY _currentVel.Y by '-1':
+                _velocity.Y *= -1;
+
+                // APPLY new Velocity to _entity.Velocity:
+                (_entity as IVelocity).Velocity = _velocity;
             }
             // SET Data Property value of _sfxCommand to "PaddleHit":
             //(_sfxCommand as ICommandOneParam<string>).Data = "PaddleHit";
@@ -174,55 +215,78 @@ namespace COMP3451Project.RIRRPackage.Behaviours
             //(_entity as ICommandSender).ScheduleCommand(_sfxCommand);
 
 
+
+            // ---------------------- Detection and interception Collision, for detecting and intercepting the player ----------
+
             // ok this bit is for the detection collision, so its the box around the npc and when
-            // the player enters this box the npc is 'aware' and starts hunting you down... theoretically
+            // the player enters this box the npc becomes 'aware' and starts hunting the player down... theoretically
+
+            // DECLARE local Vector2, name it 'tempPosition' and ASSIGN it the value of _entity.Position:
+            Vector2 tempPosition = _entity.Position;
 
             // IF moving left:
             if ((_entity as IVelocity).Velocity.X < 0)
             {
-                // IF speed DOES NOT exceed lower bound of other ICollidable's TextureSize.X - 1 as a negative:
-                // PREVENTS CLIPPING
-                //if ((_entity as IVelocity).Velocity.X >= ((pArgs.RequiredArg as ITexture).TextureSize.X - 1) * -1)
-                //{
-                //    // MINUS 0.2 multiplied by _RequiredArg's Velocity, from _velocity:
-                //    _velocity.X = _velocity.X - 0.2f * (pArgs.RequiredArg as IVelocity).Velocity.Length();
-                //}
-
-                if (((_entity as IVelocity).Velocity.X - 48) >= (pArgs.RequiredArg as ITexture).TextureSize.X - 1)
+                // IF NPC velocity.X - 3 tiles is greater than or equel to Player texture size X minus 1 pxl 
+                if (((_entity as IVelocity).Velocity.X - _tileSize) >= (pArgs.RequiredArg as ITexture).TextureSize.X - 1)
                 {
-                    
+                    // this is how to get the position of the player
+                    //(pArgs.RequiredArg as IEntity).Position 
+
+                    // ASSIGNMENT tempPosition.X has the value of the entities x velocity added to it, this is to make it move towards the player:
+                    tempPosition.X += (_entity as IVelocity).Velocity.X;
+
+                    //     player cast as IEntity                      NPC 
+                    // ((pArgs.RequiredArg as IEntity).Position.X >= _entity.Position.X)
+                   
+
+                    // ASSIGNMENT _entity.Position is set to the value of tempPosition
+                    _entity.Position = tempPosition;
+
                 }
             }
             // IF moving right:
             else if ((_entity as IVelocity).Velocity.X > 0)
             {
-                if (((_entity as IVelocity).Velocity.X + 48) <= (pArgs.RequiredArg as ITexture).TextureSize.X + 1)
+                // IF NPC velocity.X + 3 tiles is greater than or equel to Player texture size X minus 1 pxl 
+                if (((_entity as IVelocity).Velocity.X + _tileSize) <= (pArgs.RequiredArg as ITexture).TextureSize.X + 1)
                 {
-                    
+                    // ASSIGNMENT tempPosition.X has the value of the entities x velocity subtracted from it:
+                    tempPosition.X -= (_entity as IVelocity).Velocity.X;
+
+                    // ASSIGNMENT _entity.Position is set to the value of tempPosition
+                    _entity.Position = tempPosition;
                 }
             }
-            // IF moving top:
-            else if ((_entity as IVelocity).Velocity.Y < 0)
+            // IF moving up:
+            if ((_entity as IVelocity).Velocity.Y < 0)
             {
-                if (((_entity as IVelocity).Velocity.Y + 48) >= (pArgs.RequiredArg as ITexture).TextureSize.Y - 1)
+                // IF NPC velocity - 3 tiles is greater than or equel to Player texture size X minus 1 pxl 
+
+                if (((_entity as IVelocity).Velocity.Y + _tileSize) >= (pArgs.RequiredArg as ITexture).TextureSize.Y - 1)
                 {
-                    
+                    // ASSIGNMENT tempPosition.Y has the value of the entities Y velocity added to it:
+                    tempPosition.Y += (_entity as IVelocity).Velocity.Y;
+
+                    // ASSIGNMENT _entity.Position is set to the value of tempPosition
+                    _entity.Position = tempPosition;
                 }
             }
-            // IF moving bottom:
+            // IF moving down:
             else if ((_entity as IVelocity).Velocity.Y > 0)
             {
-                if (((_entity as IVelocity).Velocity.Y - 48) <= (pArgs.RequiredArg as ITexture).TextureSize.Y + 1)
+                if (((_entity as IVelocity).Velocity.Y - _tileSize) <= (pArgs.RequiredArg as ITexture).TextureSize.Y + 1)
                 {
-                    
+                    // ASSIGNMENT tempPosition.Y has the value of the entities Y velocity subtracted from it:
+                    tempPosition.Y -= (_entity as IVelocity).Velocity.Y;
+
+
+                    // ASSIGNMENT _entity.Position is set to the value of tempPosition
+                    _entity.Position = tempPosition;
                 }
             }
 
-            // MULTIPLY _currentVel.X by '-1':
-            _velocity.X *= -1;
-
-            // APPLY new Velocity to _entity.Velocity:
-            (_entity as IVelocity).Velocity = _velocity;
+            
         }
 
         #endregion
